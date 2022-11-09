a005395787d98474b9c6c30cfc519c3117d317f5
brightamouzou <brightamouzou@gmail.com>
2022-11-09 15:55:38 +0000

    v1.0.3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 5000;
+const port = 5000||process.env.PORT;
 
 let options = {
   root: __dirname,
